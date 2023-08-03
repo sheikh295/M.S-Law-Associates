5d5f9e5d7bee1f1f3f114f5f5e92f48cacc0542a
sheikh295 <sheikh295@users.noreply.github.com>
2023-08-03 16:00:27 +0500

    vite & tailwind initial setup

---
+++ main.js
@@ -0,0 +1,10 @@
+import './style.css'
+import { scrpit } from './scripts.js'
+
+document.querySelector('#app').innerHTML = `
+  <div>
+    <div></div>
+  </div>
+`
+
+script(document.querySelector('#(elementid)'))
